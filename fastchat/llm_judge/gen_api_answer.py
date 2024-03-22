342da777acef82c55164c8e0b0e5bfedc907dd01
root <jennings@mosaicml.com>
2024-03-22 03:37:24 +0000

    Fix

--- fastchat/llm_judge/gen_api_answer.py
+++ fastchat/llm_judge/gen_api_answer.py
@@ -91,7 +91,9 @@ def get_answer(
                 api_key = os.environ["DATABRICKS_REAL_TOKEN"]
                 output = db_inference_deployment(f'https://e2-dogfood.staging.cloud.databricks.com/serving-endpoints/{model}/invocations', tokenizer, conv, temperature, max_tokens, api_key=api_key, api_args={
                     'model': model,
-                    'use_raw_prompt': None
+                    'use_raw_prompt': None,
+                    'prompt': None,
+                    'messages': conv.to_openai_api_messages()
                 })
             elif "https://" in model or "http://" in model:
                 block_until_ready(model)
