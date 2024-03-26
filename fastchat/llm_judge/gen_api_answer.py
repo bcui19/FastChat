fb63cd7ebf2bfb886878039659efd85a9b1ca53e
root <jennings@mosaicml.com>
2024-03-26 02:46:00 +0000

    fix

--- fastchat/llm_judge/gen_api_answer.py
+++ fastchat/llm_judge/gen_api_answer.py
@@ -192,7 +192,7 @@ def get_answer(
                             safety_settings=safety_settings
                         )  
                     except Exception as e:
-                        time.sleep((6 - retry) * retry)
+                        time.sleep((9 - retry) * retry)
                         
                         return retry_request(retry - 1)
                     
